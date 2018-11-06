7185b80e8fcdce7b35c1066fa6b7f838c7dd5607
Bendegúz Németh <n.bendeguz3@gmail.com>
2018-11-06 15:24:36 +0100

    static tableview

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            return cell
+        } else {
+            return UITableViewCell.init()
+        }
+    }
+    
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        titleLabel.text = "Napi teendők, Napi teendők, Napi teendők, Napi teendők, Napi teendők, Napi teendők"
+        
     }
 
 
